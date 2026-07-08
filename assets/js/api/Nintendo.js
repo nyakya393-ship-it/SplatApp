f96bc1f8f93796bdccd5c1d9420c10baec7ed8eb
Sio <nyakya393@gmail.com>
2026-07-09 07:43:55 +0900

    Update Nintendo.js

--- assets/js/api/Nintendo.js
+++ assets/js/api/Nintendo.js
@@ -1,1 +1,48 @@
+// =========================
+// Nintendo API Layer
+// InkBoard
+// =========================
 
+
+
+const NintendoAPI = {
+
+
+    connected:false,
+
+
+
+    initialize(){
+
+        console.log(
+            "Nintendo API Ready"
+        );
+
+    },
+
+
+
+    async connect(){
+
+
+        /*
+            本来ここに
+            認証処理を接続
+        */
+
+
+        this.connected = true;
+
+
+        return {
+
+            success:true
+
+        };
+
+
+    }
+
+
+
+};
